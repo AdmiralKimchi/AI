6e659d8c96896c4ab93b8dd5a898ffd9bd10e56d
Mehrshad Saadatinia <saadatiniamehrshad@gmail.com>
2022-02-15 15:54:36 +0330

    noise fixed, flow works better

--- engine.py
+++ engine.py
@@ -1,6 +1,9 @@
 import pyttsx3
 from decouple import config
 from datetime import datetime
+import speech_recognition as sr
+from random import choice
+
 
 
 USERNAME = config('USER')
@@ -32,13 +35,39 @@ def greet():
     elif hour in range(16, 23):
         speak(f"Good evening {USERNAME}")
     speak(f"I am {BOTNAME}. How may I assist you?")
-    
+
+
+
+def take_command():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-us')
+        if 'exit' in query:
+            speak('take care sir, goodbye for now')
+            exit()
+        print(query)
+       
+    except Exception:
+        speak('Sorry, I could not understand. Could you please say that again?')
+        query = 'None'
+    take_command()
+    return query
+     
 
 if __name__ == '__main__':
     engine.setProperty('rate', 190)
     engine.setProperty('volume', 1.5)
     set_voice('male')
-    greet()
+    #greet()
+    take_command()
     
 
 
