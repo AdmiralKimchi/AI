adb49cde18a9c8981c02b2c79223ab5a94028280
Mehrshad Saadatinia <saadatiniamehrshad@gmail.com>
2022-02-15 13:55:33 +0330

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,31 @@
+import pyttsx3
+from decouple import config
+
+USERNAME = config('USER')
+BOTNAME = config('BOTNAME')
+
+engine = pyttsx3.init('sapi5')
+
+# Set Rate
+engine.setProperty('rate', 190)
+
+# Set Volume
+engine.setProperty('volume', 1.0)
+
+# Set Voice (Female)
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(text):
+    """Used to speak whatever text is passed to it"""
+    engine.say(text)
+    engine.runAndWait()
+
+
+if __name__ == '__main__':
+    print('hi')
+    speak('nice to meet you')
+
+
