be1d208e2f51423b13eeb8802dd210fb2a88feed
Mehrshad Saadatinia <saadatiniamehrshad@gmail.com>
2022-07-07 02:07:12 +0430

    requirements script added

---
+++ requirements.py
@@ -0,0 +1,14 @@
+import os
+os.system('cmd /c "pip install pyttsx3"')
+os.system('cmd /c "pip install SpeechRecognition"')
+os.system('cmd /c "pip install pywhatkit"')
+os.system('cmd /c "pip install wikipedia"')
+os.system('cmd /c "pip install requests"')
+os.system('cmd /c "pip install flask"')
+os.system('cmd /c "pip install nltk"')
+os.system('cmd /c "pip install python-decouple"')
+
+
+
+
+
